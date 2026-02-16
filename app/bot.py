a716c68cb7f643b94918cbca8ec112d6a4840b74
danilo645645 <sergeeffdanya@gmail.com>
2026-02-16 14:30:33 +0300

    SQLite3
    переход с in-memory (локала ) на sqlite (бд)

--- app/bot.py
+++ app/bot.py
@@ -3,9 +3,11 @@ from telebot.storage import StateMemoryStorage
 
 from .config import load_config
 from .handlers import start, start_nav, profile, wallet, support, catalog, order, chat, seller
+from .storage import init_storage
 
 def main():
     cfg = load_config()
+    init_storage()
 
     storage = StateMemoryStorage()
     bot = TeleBot(cfg.bot_token, state_storage=storage)
@@ -19,7 +21,7 @@ def main():
     catalog.register(bot)
     order.register(bot)
     chat.register(bot)
-    seller.register(bot)
+    seller.register(bot, cfg)
 
     print("Bot is running...")
     bot.infinity_polling(skip_pending=True)
