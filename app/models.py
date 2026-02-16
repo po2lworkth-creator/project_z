1cd6de5fafe50dcd03dcee3e861ef6e8eae6e8e0
danilo645645 <sergeeffdanya@gmail.com>
2026-02-16 15:08:33 +0300

    SQLite3
    переход с in-memory (локала ) на sqlite (бд)

--- app/models.py
+++ app/models.py
@@ -1,17 +1,33 @@
 from dataclasses import dataclass
 from typing import Optional
 
+
+SELLER_STATUS_NONE = "none"
+SELLER_STATUS_APPLIED = "applied"
+SELLER_STATUS_SELLER = "seller"
+SELLER_STATUS_REJECTED = "rejected"
+
+
 @dataclass
 class User:
     user_id: int
     username: Optional[str] = None
+
+    # продавец
     is_seller: bool = False
+    seller_status: str = SELLER_STATUS_NONE
+
+    # телефон
+    phone: Optional[str] = None
     seller_verified_phone: bool = False
-    balance: int = 0  # внутренняя валюта (заглушка)
+
+    # баланс
+    balance: int = 0
+
 
 @dataclass
 class Order:
     order_id: int
     buyer_id: int
     seller_id: int
-    status: str  # created / paid / delivered / confirmed / dispute
+    status: str  #
