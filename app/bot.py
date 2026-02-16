b63ffb21131872486d38fbe773fe51efc7a054d5
LimoProject <gotgotgot2281488@gmail.com>
2026-02-16 14:43:51 +0300

    Update bot.py

--- app/bot.py
+++ app/bot.py
@@ -3,11 +3,9 @@ from telebot.storage import StateMemoryStorage
 
 from .config import load_config
 from .handlers import start, start_nav, profile, wallet, support, catalog, order, chat, seller
-from .storage import init_storage
 
 def main():
     cfg = load_config()
-    init_storage()
 
     storage = StateMemoryStorage()
     bot = TeleBot(cfg.bot_token, state_storage=storage)
@@ -21,7 +19,7 @@ def main():
     catalog.register(bot)
     order.register(bot)
     chat.register(bot)
-    seller.register(bot, cfg)
+    seller.register(bot)
 
     print("Bot is running...")
     bot.infinity_polling(skip_pending=True)
